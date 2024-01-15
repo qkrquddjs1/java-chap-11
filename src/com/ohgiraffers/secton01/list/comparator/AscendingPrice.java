5044cd46354ffe13e85524687b64be181878cfec
hi <qkrquddjs9752@naver.com>
2024-01-15 18:03:57 +0900

    24.01.15

---
+++ src/com/ohgiraffers/secton01/list/comparator/AscendingPrice.java
@@ -0,0 +1,33 @@
+package com.ohgiraffers.secton01.list.comparator;
+
+import com.ohgiraffers.secton01.list.dto.BookDTO;
+
+import java.util.Comparator;
+
+/* Comparator 사용 시 제네릭 선언을 함꼐 해주어야 compare 메소드의 매개변수 타입이 정의 된다.
+* Object 타입인 경우 다운캐스팅해서 사용해야 하므로 제네릭 타입을 결정해주고 사용하는 것이 좋다. */
+public class AscendingPrice implements Comparator<BookDTO> { //<- 제네릭타입
+
+    /* sort() 메소드에서 내부적으로 compare(컴페어) 메소드를 호출하여 swap(스왑) 여부를 결정한다.
+     * Comparator 인터페이스를 상속 받으면 compare 메소드 오버라이딩이 강제화 된다.  */
+    @Override
+    public int compare(BookDTO o1, BookDTO o2) {
+
+        /* 비교 대상 두 인스턴스의 가격이 오름차순 정렬 되기 위해서는 앞의 가격이 더 작은 가격이어야 한다,
+        * 만약 뒤의 가격이 더 작은 경우에는 두 인스턴스의 순서를 부꾸어야 한다.
+        * 그 때 두값을 바꾸라는 신호로 양수를 반환하면 정렬 시 순서를 바꾸는 조건으로 사용 된다.*/
+
+        int result = 0;
+
+        if(o1.getPrice() > o2.getPrice()){
+            result = 1;
+        } else if (o1.getPrice() < o2.getPrice()){
+            result = -1;
+        } else {
+            result = 0;
+        }
+
+        return result;
+    }
+
+}
---
+++ src/com/ohgiraffers/secton01/list/run/Application02.java
@@ -0,0 +1,95 @@
+package com.ohgiraffers.secton01.list.run;
+
+import com.ohgiraffers.secton01.list.dto.BookDTO;
+import com.ohgiraffers.secton01.list.comparator.AscendingPrice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Application02 {
+    public static void main(String[] args) {
+        /* ArrayList 사용자 정의 자료형으로 이용하기, 정렬하기 */
+
+        /* 여러 권의 책 목록을 관리할 ArrayList 생성 */
+        List<BookDTO> bookList = new ArrayList<>();
+
+
+        bookList.add(new BookDTO(1, "홍길동전", "허균", 50000));
+        bookList.add(new BookDTO(2, "목민심서", "정약용", 30000));
+        bookList.add(new BookDTO(3, "동의보감", "허준", 40000));
+        bookList.add(new BookDTO(4, "삼국사기", "김부식", 46000));
+        bookList.add(new BookDTO(5, "삼국유사", "일연", 58000));
+
+//        System.out.println("bookList : " + bookList); //이렇게 쓰면 길이가 길어진다
+        /* 향상된 for문 이용해서 bookList 순회 출력 */
+        for(BookDTO book : bookList ){
+            System.out.println(book);
+        }
+
+        /* 제네릭의 타입 제한에 의해 Comparable 타입을 가지고 있는 경우에만 srot가 가능하다.
+        * EX) String 클래스는 implements Comparable (정렬 기중을 구현) 되어 있었기 떄문에 사용 가능했다. */
+//        Collections.sort(bookList); // <- 이렇게 쓸경우 상속이 안돼서 사용불가능.
+
+        /* 가격 오름차순 정렬
+        *  AscendingPrice 클래스 생성 (Comparator 인터페이스를 구현하여 정렬 기준을 작성한다.)
+        * */
+        Collections.sort(bookList, new AscendingPrice());
+        System.out.println("가격 오름차순 정렬 ================================== = = = ===");
+        for (BookDTO book : bookList){
+            System.out.println(book);
+        }
+
+        /* 가격 내림차순 정렬
+        *  정렬 기준을 계속해서 사용하는 경우에니느 AscendingPrice 클래스를 만든것 처럼 클래스를 생성해도 되지만
+        *  한번만 사용하기 위해서는 조금 더 간편하게 익명 클래스(Anonymous class)를 사용할 수 있다.
+        * */
+        Collections.sort(bookList, new Comparator<BookDTO>() {  //일회용으로 쓸수잇다.
+            @Override
+            public int compare(BookDTO o1, BookDTO o2) {
+                /* 가격 내림차순 정렬 조건
+                * o2의 가격이 더큰 경우에 양수를 반환하여 swap하라는 flag를 전달한다. */
+//                return o1.getPrice() >= o2.getPrice() ? -1 : 1;
+                return o2.getPrice() - o1.getPrice();
+            }
+        });
+        System.out.println("가격 내림차순 정렬 ================================== = = = ===");
+        for (BookDTO book : bookList){
+            System.out.println(book);
+        }
+
+        /* 제목 오름차순 정렬 */
+        bookList.sort(new Comparator<BookDTO>() {
+            @Override
+            public int compare(BookDTO o1, BookDTO o2) {
+                /* 문자열 대소비교는 String 클래스에 정의 된 compareTo() 메소드를 활용한다.
+                * 앞의 값이 더 작은 경우 음수를 반환하고 같으면 0을 반환하며 앞의 값이 더 큰 경우 양수를 반환한다. */
+                return o1.getTitle().compareTo(o2.getTitle()) ;
+            }
+        });
+        System.out.println("제목 오름차순 정렬 ================================== = = = ===");
+        for (BookDTO book : bookList){
+            System.out.println(book);
+        }
+
+        /* 제목 내림차순 정렬 */
+        bookList.sort(new Comparator<BookDTO>() {
+            @Override
+            public int compare(BookDTO o1, BookDTO o2) {
+                /* 문자열 대소비교는 String 클래스에 정의 된 compareTo() 메소드를 활용한다.
+                 * 앞의 값이 더 작은 경우 음수를 반환하고 같으면 0을 반환하며 앞의 값이 더 큰 경우 양수를 반환한다. */
+//                return o2.getTitle().compareTo(o1.getTitle()) ; // 앞뒤를 바꿔도 되지만 앞에다가 -붙히면 내림차순으로 정렬된다.
+                return -o1.getTitle().compareTo(o2.getTitle()) ;
+            }
+        });
+        System.out.println("제목 내림차순 정렬 ================================== = = = ===");
+        for (BookDTO book : bookList){
+            System.out.println(book);
+        }
+
+
+
+
+    }
+}
